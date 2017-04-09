543277bfee314da4624e8dbb4bfae172af8a68f4
wxtsdi <wxtsdi@163.com>
2017-04-09 16:26:22 +0800

    TemplateRef和ViewContainerRef动态添加内容到模板

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewContainerRef,ViewRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: "my-app",
     templateUrl: "./app.component.html",
     styles: ["width:500px;height:30px;background-color:blue"]
 })
-export class AppComponent {
-    private msgFromChild:string="";
-    showMessage(obj:any) {
+export class AppComponent  {
+    private msgFromChild: string = "";
+
+    @ViewChild('rectTemp') tempRect:TemplateRef<any>;
+    @ViewChild('rectTemp',{read:ViewContainerRef}) vcRef:ViewContainerRef;
+    //通过模板局部变量rectTemp可以返回一个ElementRef对象（这里的template）和一个ViewContainerRef对象，如果不加{read:ViewContainerRef}
+    //返回的是ElementRef对象，如果加上{read:ViewContainerRef}返回才是ViewContainerRef对象
+
+
+    showMessage(obj: any) {
         console.log("父模块开始接收");
-        this.msgFromChild=obj;
+        this.msgFromChild = obj;
     }
+
+    addChildComp() {
+        console.log("add child component");
+        let vr:ViewRef= this.vcRef.createEmbeddedView(this.tempRect);
+        console.dir(vr);
+    }
+
+
+
+
+
 }
